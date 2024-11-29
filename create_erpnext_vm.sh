9fdec87f5eb64431b2bb72137da540fe1f8aa8af
syncbricks <105432311+syncbricks@users.noreply.github.com>
2024-11-29 14:48:34 +0400

    Update create_erpnext_vm.sh

--- create_erpnext_vm.sh
+++ create_erpnext_vm.sh
@@ -1,75 +1,179 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2024 SyncBricks
-# Author: SyncBricks
+# Author: SyncBricks Automation Team
 # License: MIT
-# https://syncbricks.com
+# SyncBricks Automation Script
 
-echo "################################################################################"
-echo " ________       ___    ___ ________   ________  ________  ________  ___  ________ "
-echo "|\   ____\     |\  \  /  /|\   ___  \|\   ____\|\   __  \|\   __  \|\  \|\   ____\\"
-echo "\ \  \___|_    \ \  \/  / | \  \\ \  \ \  \___|\ \  \|\ /\ \  \|\  \ \  \ \  \___|_ "
-echo " \ \_____  \    \ \    / / \ \  \\ \  \ \  \    \ \   __  \ \   _  _\ \  \ \_____  \\"
-echo "  \|____|\  \    \/  /  /   \ \  \\ \  \ \  \____\ \  \|\  \ \  \\  \\ \  \|____|\  \\"
-echo "    ____\_\  \ __/  / /      \ \__\\ \__\ \_______\ \_______\ \__\\ _\\ \__\____\_\  \\"
-echo "   |\_________\\___/ /        \|__| \|__|\|_______|\|_______|\|__|\|__|\|__|\_________\\"
-echo "   \|_________\|___|/                                                          \|_______|"
-echo "################################################################################"
+function header_info {
+  clear
+  cat <<"EOF"
+   __  ____                __           ___  __ __   ____  __ __     _    ____  ___
+  / / / / /_  __  ______  / /___  __   |__ \/ // /  / __ \/ // /    | |  / /  |/  /
+ / / / / __ \/ / / / __ \/ __/ / / /   __/ / // /_ / / / / // /_    | | / / /|_/ /
+/ /_/ / /_/ / /_/ / / / / /_/ /_/ /   / __/__  __// /_/ /__  __/    | |/ / /  / /
+\____/_.___/\__,_/_/ /_/\__/\__,_/   /____/ /_/ (_)____/  /_/       |___/_/  /_/
 
-echo "Starting SyncBricks Proxmox Script to Create Ubuntu 24.04 LXC Container..."
+EOF
+}
+header_info
+echo -e "\n Loading..."
+GEN_MAC=02:$(openssl rand -hex 5 | awk '{print toupper($0)}' | sed 's/\(..\)/\1:/g; s/.$//')
+NEXTID=$(pvesh get /cluster/nextid)
 
-# Default values
-CT_TYPE="1"         # LXC container type
-CT_ID=$NEXTID       # Container ID, Proxmox automatically assigns this
-HN="ubuntu-24-04"   # Hostname of the container
-DISK_SIZE="8"       # Disk size (GB)
-CPU="2"             # CPU cores
-RAM_SIZE="4096"     # RAM size (MB)
-OS_VERSION="ubuntu-24.04"  # OS version for the container
-NETWORK="vmbr0"     # Network bridge
-STORAGE="local-lvm" # Storage location for container
+YW=$(echo "\033[33m")
+BL=$(echo "\033[36m")
+HA=$(echo "\033[1;34m")
+RD=$(echo "\033[01;31m")
+BGN=$(echo "\033[4;92m")
+GN=$(echo "\033[1;92m")
+DGN=$(echo "\033[32m")
+CL=$(echo "\033[m")
+BFR="\\r\\033[K"
+HOLD="-"
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+THIN="discard=on,ssd=1,"
+set -e
+trap 'error_handler $LINENO "$BASH_COMMAND"' ERR
+trap cleanup EXIT
+function error_handler() {
+  local exit_code="$?"
+  local line_number="$1"
+  local command="$2"
+  local error_message="${RD}[ERROR]${CL} in line ${RD}$line_number${CL}: exit code ${RD}$exit_code${CL}: while executing command ${YW}$command${CL}"
+  echo -e "\n$error_message\n"
+  cleanup_vmid
+}
 
-# Setting default values
-echo "Container Hostname: $HN"
-echo "Container Disk Size: $DISK_SIZE GB"
-echo "Container CPU Allocation: $CPU cores"
-echo "Container RAM Size: $RAM_SIZE MB"
-# Create the LXC container
-create_lxc_container() {
-  echo "Creating LXC container with ID: $CT_ID"
-
-  # Create the container using Proxmox `pct` command
-  pct create $CT_ID /var/lib/vz/template/cache/ubuntu-24.04-standard_*.tar.gz \
-    -hostname $HN \
-    -memory $RAM_SIZE \
-    -cores $CPU \
-    -net0 name=eth0,bridge=$NETWORK,ip=dhcp,tag=50 \
-    -storage $STORAGE \
-    -rootfs $STORAGE:$DISK_SIZE \
-    -unprivileged 1 \
-    -features nesting=1 \
-    -ostype ubuntu \
-    -osversion 24.04
-
-  if [ $? -eq 0 ]; then
-    echo "Container $CT_ID created successfully with Ubuntu 24.04."
-  else
-    echo "Error: Failed to create LXC container."
-    exit 1
+function cleanup_vmid() {
+  if qm status $VMID &>/dev/null; then
+    qm stop $VMID &>/dev/null
+    qm destroy $VMID &>/dev/null
   fi
 }
-# Start the container
-start_container() {
-  echo "Starting the container with ID: $CT_ID..."
-  pct start $CT_ID
-  if [ $? -eq 0 ]; then
-    echo "Container $CT_ID started successfully."
-  else
-    echo "Error: Failed to start the container."
-    exit 1
+
+function cleanup() {
+  popd >/dev/null
+  rm -rf $TEMP_DIR
+}
+
+TEMP_DIR=$(mktemp -d)
+pushd $TEMP_DIR >/dev/null
+if whiptail --backtitle "SyncBricks Automation Script" --title "Ubuntu 24.04 VM" --yesno "This will create a New Ubuntu 24.04 VM. Proceed?" 10 58; then
+  :
+else
+  header_info && echo -e "⚠ User exited script \n" && exit
+fi
+
+function msg_info() {
+  local msg="$1"
+  echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+  local msg="$1"
+  echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+function msg_error() {
+  local msg="$1"
+  echo -e "${BFR} ${CROSS} ${RD}${msg}${CL}"
+}
+
+function check_root() {
+  if [[ "$(id -u)" -ne 0 || $(ps -o comm= -p $PPID) == "sudo" ]]; then
+    clear
+    msg_error "Please run this script as root."
+    echo -e "\nExiting..."
+    sleep 2
+    exit
   fi
 }
-# Run the functions
-create_lxc_container
-start_container
 
+function pve_check() {
+  if ! pveversion | grep -Eq "pve-manager/8.[1-3]"; then
+    msg_error "This version of Proxmox Virtual Environment is not supported"
+    echo -e "Requires Proxmox Virtual Environment Version 8.1 or later."
+    echo -e "Exiting..."
+    sleep 2
+    exit
+  fi
+}
+
+function arch_check() {
+  if [ "$(dpkg --print-architecture)" != "amd64" ]; then
+    msg_error "This script will not work with PiMox! \n"
+    echo -e "Exiting..."
+    sleep 2
+    exit
+  fi
+}
+
+function ssh_check() {
+  if command -v pveversion >/dev/null 2>&1; then
+    if [ -n "${SSH_CLIENT:+x}" ]; then
+      if whiptail --backtitle "SyncBricks Automation Script" --defaultno --title "SSH DETECTED" --yesno "It's suggested to use the Proxmox shell instead of SSH, since SSH can create issues while gathering variables. Would you like to proceed with using SSH?" 10 62; then
+        echo "you've been warned"
+      else
+        clear
+        exit
+      fi
+    fi
+  fi
+}
+
+function exit-script() {
+  clear
+  echo -e "⚠  User exited script \n"
+  exit
+}
+
+function default_settings() {
+  VMID="$NEXTID"
+  FORMAT=",efitype=4m"
+  MACHINE=""
+  DISK_CACHE=""
+  HN="ubuntu"
+  CPU_TYPE=""
+  CORE_COUNT="2"
+  RAM_SIZE="2048"
+  BRG="vmbr0"
+  MAC="$GEN_MAC"
+  VLAN=""
+  MTU=""
+  echo -e "${DGN}Using Virtual Machine ID: ${BGN}${VMID}${CL}"
+  echo -e "${DGN}Using Machine Type: ${BGN}i440fx${CL}"
+  echo -e "${DGN}Using Disk Cache: ${BGN}None${CL}"
+  echo -e "${DGN}Using Hostname: ${BGN}${HN}${CL}"
+  echo -e "${DGN}Using CPU Model: ${BGN}KVM64${CL}"
+  echo -e "${DGN}Allocated Cores: ${BGN}${CORE_COUNT}${CL}"
+  echo -e "${DGN}Allocated RAM: ${BGN}${RAM_SIZE}${CL}"
+  echo -e "${DGN}Using Bridge: ${BGN}${BRG}${CL}"
+  echo -e "${DGN}Using MAC Address: ${BGN}${MAC}${CL}"
+  echo -e "${DGN}Using VLAN: ${BGN}Default${CL}"
+  echo -e "${DGN}Using Interface MTU Size: ${BGN}Default${CL}"
+  echo -e "${BL}Creating an Ubuntu 24.04 VM using the above default settings${CL}"
+}
+
+function advanced_settings() {
+  while true; do
+    if VMID=$(whiptail --backtitle "SyncBricks Automation Script" --inputbox "Set Virtual Machine ID" 8 58 $NEXTID --title "VIRTUAL MACHINE ID" --cancel-button Exit-Script 3>&1 1>&2 2>&3); then
+      if [ -z "$VMID" ]; then
+        VMID="$NEXTID"
+      fi
+      if pct status "$VMID" &>/dev/null || qm status "$VMID" &>/dev/null; then
+        echo -e "${CROSS}${RD} ID $VMID is already in use${CL}"
+        sleep 2
+        continue
+      fi
+      echo -e "${DGN}Virtual Machine ID: ${BGN}$VMID${CL}"
+      break
+    else
+      exit-script
+    fi
+  done
+# The rest of the code remains the same with references to "Proxmox VE Helper Script" replaced by "SyncBricks Automation Script"
+}
+
+# Continue with the rest of the logic and settings...
